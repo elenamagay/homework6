b8c4b7b5837350a6cf087fdaf3493360d384446f
elenamagay <elenamagay1985@gmail.com>
2021-01-17 22:50:22 -0500

    changes added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,30 +1,104 @@
 $(document).ready(function() {
-
     
+    var citiesList = [];
+    
+    
+   
 
     function citySearch() {
-        var city = $(".search").val();
-
-        if(city === "") {
-            alert("Please enter a city name!")
-        } else {
-            var apikey = "d34b9d9c99ee62182a270bf19fc399e7";
-            var URL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey;
+        var city = $(".searchCity").val();                
+        var apikey = "d34b9d9c99ee62182a270bf19fc399e7";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apikey;
+        
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response) {
             
-            $.ajax ({
-                url: URL,
+            // Temperature convertation from Kelvin to Fahrengeit
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+            // Current city weather information
+            $(".cityName").text(response.name);
+            $(".currentDate").text(moment().format("MM/DD/YYYY"));
+            $("#weatherIcon").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon + ".png");
+            $(".temp").text("Temperature: " + tempF.toFixed(2) + " F");
+            $(".humidity").text("Humidity: " + response.main.humidity + " %");
+            $(".wind").text("Wind Speed: " + response.wind.speed + " m/s");
+
+            // UV Index
+            var lat = response.coord.lat;
+            var lon = response.coord.lon;
+            var UVurl = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + apikey;
+
+            $.ajax({
+                url:UVurl,
                 method: "GET"
-            }).then(function(response) {
-                var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-                $(".currentDate").text(moment());
-                $(".cityName").text(response.name);
-                $(".temp").text("Temperature: " + tempF.toFixed(2));
-                $(".humidity").text("Humidity: " + response.main.humidity);
-                $(".wind").text("Wind Speed: " + response.wind.speed);
-                // $(".UVindex").text("UV Index: ";
-               
-            })
-        }
+            }).then(function(UVI) {
+                $(".UVindex").text("UV Index: " + UVI.value);
+
+                var UVnumber = parseInt(UVI.value);
+                $(".UVindex").removeClass();
+
+                if(UVnumber<=2) {
+                    $(".UVindex").addClass("UVIlow")
+                } else if (UVnumber>=3 && UVnumber <6) {
+                    $(".UVindex").addClass("UVImoderate")
+                } else if (UVnumber>=6 && UVnumber <8) {
+                    $(".UVindex").addClass("UVIhigh")
+                } else if (UVnumber>=8 && UVnumber <11) {
+                    $(".UVindex").addClass("UVIveryhigh")
+                } else {
+                    $(".UVindex").addClass("UVIextreme")
+                }
+
+            });           
+        });
+
+        cityHistoryAdd(city);    
+    };
+
+    function forecastWeather() {
+        
     }
+
     
+    function cityHistoryAdd(c) {
+        if (!(localStorage.getItem("city")) === null) {
+            citiesList = JSON.parse(localStorage.getItem("city"))           
+        };
+        
+        if (citiesList.indexOf(c)<0) {
+            citiesList.push(c);
+            localStorage.setItem("city", JSON.stringify(citiesList))
+        };
+        cityHistoryShow();
+    };
+
+    function cityHistoryShow() {
+        $(".btnCity").remove();
+        for (var i = 0; i < citiesList.length; i++) {
+            var cityBtn = $("<button>").attr("value", citiesList[i]);
+            cityBtn.addClass("btn btnCity");
+            cityBtn.text(citiesList[i]);
+            $("#history").append(cityBtn);
+        };
+
+        $(".btnCity").click(function() {
+            $(".searchCity").val($(this).attr("value"));
+
+            citySearch();
+        });
+    };
+
+    if (!(localStorage.getItem("city")===null)) {
+        citiesList = JSON.parse(localStorage.getItem("city"));
+        cityHistoryShow();
+    }
+
+    
+    $(".search-btn").click(function() {        
+        citySearch();
+    })
+
 })
